ce8c0076cae8898f4f6a1b5256215758ee001402
Joshuavarghese527 <joshuavarghese527@gmail.com>
2017-06-05 21:28:46 -0400

    Created fb login, code editor, task function

--- passport.js
+++ passport.js
@@ -1,5 +1,6 @@
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
+var FacebookStrategy = require('passport-facebook').Strategy;
 
 passport.serializeUser(function (user, done) {
   done(null, user._id);
@@ -30,5 +31,43 @@ passport.use(new LocalStrategy({
 
       return done(null, user);
     })
+  }
+));
+
+passport.use(new FacebookStrategy({
+    clientID: '1780561938831148',
+    clientSecret: '8ff6f61e9d00ff4b8f8131d09b49925e',
+    callbackURL: 'http://localhost:3000/auth/facebook/callback',
+    profileFields: ['id', 'displayName', 'email']
+  },
+  function(token, refreshToken, profile, done) {
+    User.findOne({'facebookId': profile.id}, function(err, user) {
+      if (err) return done(err);
+
+      if (user) {
+        return done(null, user);
+      } else {
+        User.findOne({email: profile.emails[0].value}, function (err, user) {
+          if (user) {
+            user.facebookId = profile.id
+            return user.save(function (err) {
+              if (err) return done(null, false, { message: "Can't save user info"});
+              return done(null, user);
+            })
+          }
+
+          var user = new User();
+          user.name = profile.displayName;
+          user.email = profile.emails[0].value;
+          user.facebookId = profile.idea
+          user.save(function (err) {
+            if (err) return done(null, false, { message: "Can't save user info"});
+            return done(null, user);
+          });
+        })
+      }
+
+
+    });
   }
 ));
